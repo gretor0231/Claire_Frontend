53f1796dea63c7ef0d4330ffb0c4a4978a42331d
gretor0231 <gretor0231@gmail.com>
2024-03-14 13:03:18 +0800

    added type head to main pages.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,95 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
-
-const countries = [
-    "Alabama",
-    "Alaska",
-    "Arizona",
-    "Arkansas",
-    "California",
-    "Colorado",
-    "Connecticut",
-    "Delaware",
-    "Florida",
-    "Georgia",
-    "Hawaii",
-    "Idaho",
-    "Illinois",
-    "Indiana",
-    "Iowa",
-    "Kansas",
-    "Kentucky",
-    "Louisiana",
-    "Maine",
-    "Maryland",
-    "Massachusetts",
-    "Michigan",
-    "Minnesota",
-    "Mississippi",
-    "Missouri",
-    "Montana",
-    "Nebraska",
-    "Nevada",
-    "New Hampshire",
-    "New Jersey",
-    "New Mexico",
-    "New York",
-    "North Carolina",
-    "North Dakota",
-    "Ohio",
-    "Oklahoma",
-    "Oregon",
-    "Pennsylvania",
-    "Rhode Island",
-    "South Carolina",
-    "South Dakota",
-    "Tennessee",
-    "Texas",
-    "Utah",
-    "Vermont",
-    "Virginia",
-    "Washington",
-    "West Virginia",
-    "Wisconsin",
-    "Wyoming"
-];
+import TypeHead from "./typehead";
 
 function App() {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-
-    const handleChange = (e) => {
-        const value = e.target.value;
-        setSearchTerm(value);
-
-        const matchingCountries = countries.filter(country =>
-            country.toLowerCase().startsWith(value.toLowerCase())
-        );
-
-        setSuggestions(matchingCountries);
-    };
-
-    const handleSelect = (value) => {
-        setSearchTerm(value);
-        setSuggestions([]);
-    };
 
     return (
-        <div className="container">
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={handleChange}
-                placeholder="Search..."
-                className="search-input"
-            />
-            <ul className="suggestions">
-                {suggestions.map((country, index) => (
-                    <li key={index} onClick={() => handleSelect(country)}>{country}</li>
-                ))}
-            </ul>
+        <div className="App">
+            <header className="App-header">
+                <h3>Typeahead Example</h3>
+            </header>
+            <main>
+                <TypeHead /> {/* Render the BasicExample component */}
+            </main>
         </div>
     );
+
 }
 
 export default App;
