bdce175a6adcb53af0beac03151b8829c38d47cf
gretor0231 <gretor0231@gmail.com>
2024-03-18 12:51:27 +0800

    change port to 5000.

--- typehead-backend/server.js
+++ typehead-backend/server.js
@@ -63,7 +63,7 @@ app.get('/states', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
